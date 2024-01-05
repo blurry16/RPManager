aff37c35d9f42f8c3eea5fef0adb13f69e148cf3
blurry16 <roma.reznik@gmail.com>
2024-01-05 23:41:58 +0300

    JOBS ADDED! new command: #addjob, #resetjob, #getjob, #myjob, #payall. bugs fixed.

--- main.py
+++ main.py
@@ -41,21 +41,27 @@ if __name__ == "__main__":
                         username = line.split()[4].split("<")[1].split(">")[0]
                         money = open("money.json", "r")
                         names = open("names.json", "r")
+                        jobs = open("players_jobs.json", "r")
                         money_data = json.load(money)
                         names_data = json.load(names)
-                        if username not in money_data and username not in names_data:
+                        jobs_data = json.load(jobs)
+                        if username not in money_data and username not in names_data and username not in jobs_data:
                             money_data[username] = 500.0
                             names_data[username] = ""
+                            jobs_data[username] = ""
                             print(f"Added {username} as a valid player")
                             mcprint("Thanks for registering with RoleBOT2!")
                         else:
                             mcprint("You have already registered with RoleBOT2.")
                         money = open("money.json", "w")
                         names = open("names.json", "w")
+                        jobs = open("players_jobs.json", "w")
                         money.seek(0)
                         names.seek(0)
+                        jobs.seek(0)
                         json.dump(money_data, money, indent=2)
                         json.dump(names_data, names, indent=2)
+                        json.dump(jobs_data, jobs, indent=2)
                     elif "#balance" == line.lower().split()[5]:
                         print(line)
                         username = line.split()[4].split("<")[1].split(">")[0]
@@ -282,6 +288,134 @@ if __name__ == "__main__":
                             else:
                                 mcprint(f"/rg removemember {region_name} {nameToRemove}")
                                 mcprint(f"Successfully removed {nameToRemove}.")
+                        else:
+                            mcprint("No permissions.")
+                    elif "#addjob" == line.lower().split()[5]:
+                        print(line)
+                        command = line.split()[5]
+                        username = line.split()[4].split("<")[1].split(">")[0]
+                        if username in admins:
+                            args = line.replace("\n", "").split(f"{command} ", 1)[1].split()
+                            job_naming = args[0].lower()
+                            wage = float(args[1])
+                            available_jobs = open("available_jobs.json", "r")
+                            available_jobs_data = json.load(available_jobs)
+                            if job_naming not in available_jobs_data and wage >= 0:
+                                available_jobs_data[job_naming] = wage
+                                print(f"Added new job '{job_naming}' with {wage} magmas wage.")
+                                mcprint(f"Added '{job_naming}' job with {wage} wage.")
+                            elif wage < 0:
+                                mcprint("Wrong value. wage < 0")
+                            else:
+                                mcprint(f"Job '{job_naming}' already exist.")
+                            available_jobs = open("available_jobs.json", "w")
+                            json.dump(available_jobs_data, available_jobs, indent=2)
+                        else:
+                            mcprint("No permissions.")
+                    elif "#setjob" == line.lower().split()[5]:
+                        print(line)
+                        command = line.split()[5]
+                        username = line.split()[4].split("<")[1].split(">")[0]
+                        if username in admins:
+                            args = line.replace("\n", "").split(f"{command} ", 1)[1].split()
+                            nameToSet = mojang_api.get_username(mojang_api.get_uuid(args[0]))
+                            job_naming = args[1].lower()
+                            available_jobs = open("available_jobs.json", "r")
+                            available_jobs_data = json.load(available_jobs)
+                            players_jobs = open("players_jobs.json", "r")
+                            players_jobs_data = json.load(players_jobs)
+                            if job_naming in available_jobs_data:
+                                if nameToSet not in players_jobs_data:
+                                    mcprint(f"{nameToSet} hasn't registered yet.")
+                                else:
+                                    players_jobs_data[nameToSet] = job_naming
+                                    mcprint(f"Job '{job_naming}' set for {nameToSet}")
+                                    print(f"Job '{job_naming}' set for {nameToSet}")
+                            else:
+                                mcprint("This job doesn't exist.")
+
+                            players_jobs = open("players_jobs.json", "w")
+                            available_jobs.seek(0)
+                            players_jobs.seek(0)
+                            json.dump(players_jobs_data, players_jobs, indent=2)
+                        else:
+                            mcprint("No permissions.")
+                    elif "#resetjob" == line.lower().split()[5]:
+                        print(line)
+                        command = line.split()[5]
+                        username = line.split()[4].split("<")[1].split(">")[0]
+                        if username in admins:
+                            args = line.replace("\n", "").split(f"{command} ", 1)[1].split()
+                            nameToReset = mojang_api.get_username(mojang_api.get_uuid(args[0]))
+                            players_jobs = open("players_jobs.json", "r")
+                            players_jobs_data = json.load(players_jobs)
+                            if nameToReset not in players_jobs_data:
+                                mcprint(f"{nameToReset} hasn't registered yet.")
+                            elif players_jobs_data[nameToReset] == "":
+                                mcprint(f"{nameToReset} doesn't have a job.")
+                            else:
+                                players_jobs_data[nameToReset] = ""
+                                mcprint(f"Job reset for {nameToReset}.")
+                                print(f"Job reset for {nameToReset}.")
+                            players_jobs.seek(0)
+                            players_jobs = open("players_jobs.json", "w")
+                            json.dump(players_jobs_data, players_jobs, indent=2)
+                        else:
+                            mcprint("No permissions.")
+                    elif "#getjob" == line.lower().split()[5]:
+                        print(line)
+                        command = line.split()[5]
+                        username = line.split()[4].split("<")[1].split(">")[0]
+                        args = line.replace("\n", "").split(f"{command}", 1)[1].split()
+                        nameToGet = mojang_api.get_username(mojang_api.get_uuid(args[0]))
+                        players_jobs = open("players_jobs.json", "r")
+                        players_jobs_data = json.load(players_jobs)
+                        if nameToGet not in players_jobs_data:
+                            mcprint(f"{nameToGet} hasn't registered yet.")
+                        elif players_jobs_data[nameToGet] == "":
+                            mcprint(f"{nameToGet} doesn't have a job.")
+                        else:
+                            mcprint(f"{nameToGet}'s job is '{players_jobs_data[nameToGet]}'.")
+                        players_jobs.seek(0)
+
+                    elif "#myjob" == line.lower().split()[5]:
+                        print(line)
+                        command = line.split()[5]
+                        username = line.split()[4].split("<")[1].split(">")[0]
+                        players_jobs = open("players_jobs.json", "r")
+                        players_jobs_data = json.load(players_jobs)
+                        if username not in players_jobs_data:
+                            mcprint("You haven't registered yet.")
+                        elif players_jobs_data[username] == "":
+                            mcprint("You don't have job.")
+                        else:
+                            mcprint(f"Your job is '{players_jobs_data[username]}'.")
+                        players_jobs.seek(0)
+                    elif "#payall" == line.lower().split()[5]:
+                        print(line)
+                        command = line.split()[5]
+                        username = line.split()[4].split("<")[1].split(">")[0]
+                        if username in admins:
+                            available_jobs = open("available_jobs.json", "r")
+                            available_jobs_data = json.load(available_jobs)
+                            players_jobs = open("players_jobs.json", "r")
+                            players_jobs_data = json.load(players_jobs)
+                            money = open("money.json", "r")
+                            money_data = json.load(money)
+                            for player in players_jobs_data:
+                                player_job = players_jobs_data[player]
+                                if player_job == "":
+                                    pass
+                                else:
+                                    money_data[player] += available_jobs_data[player_job]
+                                    print(f"Salary paid to {player}.")
+                            money = open("money.json", "w")
+                            available_jobs.seek(0)
+                            players_jobs.seek(0)
+                            money.seek(0)
+                            json.dump(money_data, money, indent=2)
+                            mcprint("Salary was successfully paid to all players.")
+                            print("Salary was successfully paid to all players.")
                         else:
                             mcprint("No permissions.")
                     elif "#github" == line.lower().split()[5]:
